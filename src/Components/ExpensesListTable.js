dc5adc237038b5883df82e308f1b08e07e1a5317
Ignacio Aristo <ignacioaristo@gmail.com>
2021-04-04 22:22:06 -0300

    added date

--- src/Components/ExpensesListTable.js
+++ src/Components/ExpensesListTable.js
@@ -1,11 +1,15 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import { MdDelete } from "react-icons/md";
 
 const ExpensesListTable = (props) => {
+
+  const [date, setDate] = useState(new Date().toLocaleDateString());
+
   return (
     <div>
       <Fragment>
       <tr className="expense-line-table-tr">
+          <td className="table-data">{date}</td>
           <td className="table-data">{props.item.concept}</td>
           <div className="table-data-second-column">
             <td className="table-data">{`$${props.item.amount}`}</td>
